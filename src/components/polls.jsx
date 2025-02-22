63c64ffd1940506c5a2b70fff117af610d389775
NostalgicWinters <Dev.Sterling@outlook.com>
2025-02-22 20:05:40 +0530

    Update polls.jsx

--- src/components/polls.jsx
+++ src/components/polls.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Hero from './Hero'
+import { Link } from 'react-router-dom'
+import Card from './Card'
 
-function polls() {
+function Polls() {
   return (
-    <div>polls</div>
+    <div>
+      <Hero />
+      <Card />
+    </div>
   )
 }
 
-export default polls
+export default Polls
