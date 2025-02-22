f760976ff6bce4d213131831c4371a9ee36e24fc
NostalgicWinters <Dev.Sterling@outlook.com>
2025-02-23 02:20:39 +0530

    Update polls.jsx

--- src/components/polls.jsx
+++ src/components/polls.jsx
@@ -8,10 +8,14 @@ function Polls() {
     <div>
       <Hero />
       <div className='flex justify-center'>
-        <Card />
-        <Card />
-        <Card />
+        <Card location="Mumbai" disasterType="tsunami" />
+        <Card location="Mt. Fuji" disasterType="volcano" />
+        <Card location="Delhi" disasterType="earthquake" />
       </div>
+      <Link to='/Addpolls' className='text-white bg-red-600 py-2.5 px-5 rounded-2xl flex justify-self-center justify-center my-4'>
+        <span className='font-bold align-middle mx-2'>+</span>
+        <text className='align-middle'>Add Post</text>
+      </Link>
     </div>
   )
 }
