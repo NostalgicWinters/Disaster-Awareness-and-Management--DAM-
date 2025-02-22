15d3a6bdc49341ae8954b56ff88e5a3d24752361
Anmol Upadhyay <uanmol47@gmail.com>
2025-02-22 21:25:22 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,6 +9,8 @@ function About() {
         <div className='bg-black blur-xs' style={{zIndex:-1, width:"90%", height:"100%", position:"absolute", opacity:"70%", margin:"3rem", marginTop:"1.75rem" }} ></div>
         <div className=' text-white' style={{marginTop:"2rem", marginBottom:"4rem", marginLeft:"4rem", marginRight:"8rem"}}>
             <h1 className='text-white text-5xl font-bold m-4'>About Us</h1>
+
+            
             <h2 className='font-bold text-3xl m-2 mt-4'>Our Mission</h2>
             <p className='m-2 text-xl' >At Disaster Awareness and Management (DAM), we aim to leverage technology to enhance disaster preparedness, response, and relief efforts. Our platform aggregates real-time data from wikipedia and other sources to detect and track natural disasters, providing timely and accurate information to those in need.</p>
             <h2 className='font-bold text-3xl m-2 mt-4'>What we do:</h2>
@@ -21,6 +23,9 @@ function About() {
             <p className='m-2 text-xl' >In times of crisis, speed and accuracy can save lives. By harnessing the power of social media and AI, we provide a centralized, accessible, and real-time disaster awareness system for individuals, organizations, and first responders.</p>
             <h2 className='font-bold text-3xl m-2 mt-4'>Join Us</h2>
             <p className='m-2 text-xl' >We are committed to improving disaster response through innovation and collaboration. Whether you're a developer, researcher, or someone passionate about disaster relief, we welcome your contributions to make this platform even more impactful.</p>
+            <h2 className='font-bold text-3xl m-2 mt-4'>Crafted By Visionary Minds</h2>
+            <p className='m-2 text-xl' >Dev Verma, Anmol Upadhyay, Aayush Gautam </p>
+            
         </div>
     </div>
   )
