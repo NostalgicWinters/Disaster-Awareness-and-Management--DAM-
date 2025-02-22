ae16c83a051690f1343840526b53ca2c60285ab4
NostalgicWinters <Dev.Sterling@outlook.com>
2025-02-23 04:36:16 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -34,7 +34,7 @@ function Login() {
         <div className='bg-black opacity-70 flex flex-col justify-between absolute' style={{left:"35%", top:"25%", borderRadius:"5%"}}>
             <h1 className='text-white text-3xl p-5 mx-20'>Login</h1>
             <input type='text' placeholder='Email' className='text-white p-4 mr-20 ml-5 my-2' value={email} onChange={(event)=>{setEmail(event.target.value)}}></input>
-            <Link to="/registration" className='clickableLinks'><span className='text-blue-600 underline p-4 mr-20 ml-5 my-2'>First time user? Register here</span></Link>
+            <Link to="/registeration" className='clickableLinks'><span className='text-blue-600 underline p-4 mr-20 ml-5 my-2'>First time user? Register here</span></Link>
             <button className='text-white bg-red-700 p-4 ml-4 mr-4 mt-4 mb-6 registerBtn' style={{borderRadius:"1000px"}} onClick={handleLogin}>Login</button>
         </div>
     </div>
