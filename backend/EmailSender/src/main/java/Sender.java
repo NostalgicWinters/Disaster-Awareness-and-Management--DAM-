8db3d7c20b88649f7b3f2381aa600c3a38f47410
Aayush Gautam <aayush_2006@outlook.com>
2025-02-23 13:28:38 +0530

    Sender.java

--- backend/EmailSender/src/main/java/Sender.java
+++ backend/EmailSender/src/main/java/Sender.java
@@ -1,27 +1,10 @@
-import java.util.List;
+import java.io.IOException;
 
-public class Sender 
-{
-    public static void main(String[] args) 
-    {
-        try 
-         {
-            // Fetch Email from MongoDB Atlas
-            List<String> emails = SendMailWithAttachment.fetchEmailsFromMongoDB();
-            
-            // Create a single instance of SendMailWithAttachment
-            SendMailWithAttachment sendMailWithAttachment = new SendMailWithAttachment();
 
-            // Send email to each user
-            for (String email : emails) 
-            {
-                sendMailWithAttachment.send(email);
-            }
-            
-         } 
-        catch (Exception e) 
-            {
-            e.printStackTrace();
-            }
+public class Sender {
+
+	public static void main(String[] args) throws IOException {
+        new SendMailWithAttachment().sendEmails();
     }
+
 }
